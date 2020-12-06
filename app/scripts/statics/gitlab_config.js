7bdf299c3095a2559b2d2214b9ddebaf348b1e34
machong.618 <machong.618@bytedance.com>
2020-12-06 18:50:42 +0800

    feat: init

--- app/scripts/statics/gitlab_config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  templateRepo: "git@code.byted.org:ev_game/game-template.git",
-  gameRepoGrounpName: "ev_game",
-  employeePermissions: { "maxiyue@bytedance.com": "owner", "wangziyi.2019@bytedance.com": "owner", "gongrui@bytedance.com": "owner", "xingguang@bytedance.com": "owner"},
-  groupPermissions: { "55181": "developer" },
-  departmentId: 288980000064981408
-}
